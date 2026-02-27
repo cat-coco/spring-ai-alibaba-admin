4e54b41dde33fda0ea4952adf85b04be63a5f7fd
liusuihui <liusuihui@xiaomi.com>
2026-02-28 01:51:17 +0800

    local

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 cd docker/middleware/
 docker compose up -d --build
 sleep 5
-cd ../../spring-ai-alibaba-admin-server-start
-mvn clean install -DskipTests
+cd ../..
+mvn clean install -DskipTests -U
 
